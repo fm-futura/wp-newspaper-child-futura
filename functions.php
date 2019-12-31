cec88f17eb80afb153cd63be6beef2d338d68a28
Adrián Pardini <github@tangopardo.com.ar>
2019-12-31 08:36:50 -0300

    adds basic theme skeleton

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 1001);
+function theme_enqueue_styles() {
+    wp_enqueue_style('td-theme', get_template_directory_uri() . '/style.css', '', TD_THEME_VERSION, 'all' );
+    wp_enqueue_style('td-theme-child', get_stylesheet_directory_uri() . '/style.css', array('td-theme'), TD_THEME_VERSION . 'c', 'all' );
+
+}
